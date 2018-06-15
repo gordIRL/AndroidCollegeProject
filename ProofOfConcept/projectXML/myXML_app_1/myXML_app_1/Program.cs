8156edd2145cc15915cc4e33bab4e8c36ada3acb
gordIRL <gord.leary@gmail.com>
2018-06-15 09:47:57 +0100

    XML app updated

--- ProofOfConcept/projectXML/myXML_app_1/myXML_app_1/Program.cs
+++ ProofOfConcept/projectXML/myXML_app_1/myXML_app_1/Program.cs
@@ -13,41 +13,30 @@ namespace myXML_app_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-
+            // select xml file to read
             XDocument xmlFile = XDocument.Load("ff_calendar_thisweek.xml");
 
-            //Console.WriteLine(xmlFile.Root.ToString()); 
 
-            // displays everything
-            //foreach (var item in xmlFile.Root.Elements())
-            //{
-            //    Console.WriteLine(item.Value.Trim() + "  ");
-            //}
+            Console.WriteLine("\n\nView original XML data - Press ENTER");
+            Console.ReadLine();
+            // display xml file - original format, includes tags etc
+            Console.WriteLine(xmlFile.Root.ToString());
+            
 
-
-            //// display all 
-            ////foreach (var item in xmlFile.Descendants("event").Elements("title"))
-            //foreach (var item in xmlFile.Descendants("event"))
-            //{
-            //    Console.WriteLine(
-            //                        item.Element("country").Value  + ":  \t"  +
-            //                        item.Element("impact").Value   + ":    \t"  +
-            //                        item.Element("date").Value     + ":    \t" +
-            //                        item.Element("time").Value     + ":    \t" +
-            //                        item.Element("title").Value    
-            //                      );                         // .Value - removes surrounding tags
-            //}
+            Console.WriteLine("\n\nView raw XML data - Press ENTER");
+            Console.ReadLine();
+            //displays all raw data in XML file
+            foreach (var item in xmlFile.Root.Elements())
+            {
+                Console.WriteLine(item.Value.Trim() + "  ");
+            }
 
 
 
-            var highestImpact = from myVar in xmlFile.Descendants("event")
-                                where myVar.Element("impact").Value == "High"     && 
-                                (myVar.Element("country").Value == "GBP"
-                                || myVar.Element("country").Value == "USD")
-                                select myVar;
-
-            foreach (var item in highestImpact)
+            Console.WriteLine("\n\nView XML data - formatted - Press ENTER");
+            Console.ReadLine();          
+            // display all XML data - formatted             
+            foreach (var item in xmlFile.Descendants("event"))
             {
                 Console.WriteLine(
                                     item.Element("country").Value + ":  \t" +
@@ -60,7 +49,29 @@ namespace myXML_app_1
 
 
 
+            Console.WriteLine("\n\nView LINQ queried XML data - Press ENTER");
+            Console.ReadLine();
+            // sample selection using LINQ - GBP & USD currencies with 'High' impact status
+            var highestImpact = from myVar in xmlFile.Descendants("event")
+                                where myVar.Element("impact").Value == "High" &&
+                                (myVar.Element("country").Value == "GBP"
+                                || myVar.Element("country").Value == "USD")
+                                select myVar;
 
+
+            // display the result of LINQ query
+            foreach (var item in highestImpact)
+            {
+                Console.WriteLine(
+                                    item.Element("country").Value + ":  \t" +
+                                    item.Element("impact").Value + ":    \t" +
+                                    item.Element("date").Value + ":    \t" +
+                                    item.Element("time").Value + ":    \t" +
+                                    item.Element("title").Value
+                                  );                         // .Value - removes surrounding tags
+            }
+            // Pause before ending program
+            Console.WriteLine("\n\nExit Program - Press ENTER");
             Console.ReadLine();
         }
     }
