7c95eed36122e4db5d1107fdc8d22b831f6d02d9
gordIRL <gord.leary@gmail.com>
2018-06-18 11:34:38 +0100

    Splash screen added

---
+++ CurrencyAlertApp/CurrencyAlertApp/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+//  ??
+using Android.Support.V7.Widget;
+
+
+
+
+namespace CurrencyAlertApp
+{
+    [Activity(Theme = "@style/MyTheme.Base", Label = "CurrencyAlertApp")]  // removed (MainLauncher = true)  // must have an appCompat theme
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
