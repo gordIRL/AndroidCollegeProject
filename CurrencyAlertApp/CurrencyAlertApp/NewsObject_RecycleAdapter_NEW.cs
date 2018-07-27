2aeded55cfe68daaedd5ecd8058ba8ffd208fd34
gordIRL <gord.leary@gmail.com>
2018-07-27 22:44:38 +0100

    Updated Recycle View test

---
+++ CurrencyAlertApp/CurrencyAlertApp/NewsObject_RecycleAdapter_NEW.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using CurrencyAlertApp.DataAccess;
+
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// to pass a XDocument add reference:    System.Xml.Linq.
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.IO;
+using System.Reflection;
+using Android.Content.Res;
+using Android.Util;
+using System.Collections.Generic;
+
+namespace CurrencyAlertApp
+{
+    // Adapter to connect the data set (photo album) to the RecyclerView: 
+    public class NewsObject_RecycleAdapter_NEW : RecyclerView.Adapter
+    {
+        public event EventHandler<NewsObject_RecycleAdapter_NEWClickEventArgs> ItemClick;
+        public event EventHandler<NewsObject_RecycleAdapter_NEWClickEventArgs> ItemLongClick;
+
+        // Underlying data set (a List<newsObject>):
+        public List<NewsObject> mNewsObjectList;
+
+        // Load the adapter with the data set (List<newsObject>) at construction time:
+        public NewsObject_RecycleAdapter_NEW(List<NewsObject> newsObjectList)
+        {
+            mNewsObjectList = newsObjectList;
+        }
+
+
+
+
+
+        // Create a new views / photo CardView (invoked by the layout manager): 
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            //Setup your layout here
+            // Inflate the CardView for the photo:
+            View itemView = LayoutInflater.From(parent.Context).
+                        Inflate(Resource.Layout.PhotoCardView, parent, false);
+
+            // Create a ViewHolder to find and hold these view references, and 
+            // register OnClick with the view holder:
+            //NewsObject_ViewHolder vh = new NewsObject_ViewHolder(itemView, OnClick); (original)
+            var vh = new NewsObject_RecycleAdapter_NEW_ViewHolder(itemView, OnClick, OnLongClick);       
+            return vh;
+        }
+
+
+
+
+        // Fill in the contents of a view / the photo card (invoked by the layout manager):
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            // Replace the contents of the view with that element          
+            NewsObject_RecycleAdapter_NEW_ViewHolder vh = viewHolder as NewsObject_RecycleAdapter_NEW_ViewHolder;
+            
+            // Set the ImageView and TextView in this ViewHolder's CardView 
+            // from this position in the List<newsObject> (photo album)
+
+            //  Assign content (get currency icon 1st)
+            string countryChar = mNewsObjectList[position].CountryChar.ToString().ToUpper();
+            int imageID = GetImageForCurrency(countryChar);
+            vh.Icon.SetImageResource(imageID);            
+
+            //  Assign content - continued
+            vh.Caption1.Text = mNewsObjectList[position].CountryChar;
+            vh.Caption2.Text = mNewsObjectList[position].MarketImpact;
+        }
+
+
+        // Return the number of photos available in the photo album:
+        public override int ItemCount =>  mNewsObjectList.Count;
+
+        void OnClick(NewsObject_RecycleAdapter_NEWClickEventArgs args) => ItemClick?.Invoke(this, args);
+        void OnLongClick(NewsObject_RecycleAdapter_NEWClickEventArgs args) => ItemLongClick?.Invoke(this, args);
+
+
+
+        public int GetImageForCurrency(string currentCurrency)
+        {
+            int imageID = 0;
+
+            switch (currentCurrency)
+            {
+                case "USD":
+                    imageID = Resource.Drawable.bears;
+                    break;
+                case "GBP":
+                    imageID = Resource.Drawable.dogbasket;
+                    break;
+                case "CAD":
+                    imageID = Resource.Drawable.elephant;
+                    break;
+                case "JPY":
+                    imageID = Resource.Drawable.gerbil;
+                    break;
+                case "CNY":
+                    imageID = Resource.Drawable.Kangaroo;
+                    break;
+                case "NZD":
+                    imageID = Resource.Drawable.kitten;
+                    break;
+                case "AUD":
+                    imageID = Resource.Drawable.penguin;
+                    break;
+                case "CHF":
+                    imageID = Resource.Drawable.snonleopard;
+                    break;
+                case "EUR":
+                    imageID = Resource.Drawable.whaleshark;
+                    break;
+                case "Events that effect All":
+                    imageID = Resource.Drawable.musicalnotesheadphoneColor;
+                    break;
+                default:
+                    imageID = Resource.Drawable.musicalnotesheadphoneColor;
+                    break;
+            }
+            return imageID;
+        }
+
+
+    }
+
+
+
+
+    public class NewsObject_RecycleAdapter_NEW_ViewHolder : RecyclerView.ViewHolder
+    {
+        //public TextView TextView { get; set; }
+        public ImageView Icon { get; private set; }
+        public TextView Caption1 { get; private set; }
+        public TextView Caption2 { get; private set; }
+
+        // Get references to the views defined in the CardView layout.
+        public NewsObject_RecycleAdapter_NEW_ViewHolder(View itemView, 
+                        Action<NewsObject_RecycleAdapter_NEWClickEventArgs> clickListener,
+                        Action<NewsObject_RecycleAdapter_NEWClickEventArgs> longClickListener) : base(itemView)
+
+        ////public NewsObject_RecycleAdapter_NEWViewHolder(View itemView, Action<int> listener) : base(itemView)
+        {
+            //TextView = v;
+            // Locate and cache view references:
+            Icon = itemView.FindViewById<ImageView>(Resource.Id.imageView);
+            Caption1 = itemView.FindViewById<TextView>(Resource.Id.textView);
+            Caption2 = itemView.FindViewById<TextView>(Resource.Id.textView222);
+
+            // Detect user clicks on the item view and report which item was clicked 
+            itemView.Click += (sender, e) => clickListener(new NewsObject_RecycleAdapter_NEWClickEventArgs { View = itemView, Position = AdapterPosition });
+            itemView.LongClick += (sender, e) => longClickListener(new NewsObject_RecycleAdapter_NEWClickEventArgs { View = itemView, Position = AdapterPosition });
+        }
+    }
+
+    public class NewsObject_RecycleAdapter_NEWClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
--- CurrencyAlertApp/CurrencyAlertApp/TestRecycleActivity.cs
+++ CurrencyAlertApp/CurrencyAlertApp/TestRecycleActivity.cs
@@ -8,37 +8,65 @@ using Android.OS;
 using Android.Support.V7.Widget;
 using System.Collections.Generic;
 
+
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+using CurrencyAlertApp.DataAccess;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// to pass a XDocument add reference:    System.Xml.Linq.
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.IO;
+using System.Reflection;
+using Android.Content.Res;
+using Android.Util;
+
 namespace CurrencyAlertApp
 {
     [Activity(Label = "RecyclerViewer", MainLauncher = true, Icon = "@drawable/icon",
                Theme = "@android:style/Theme.Material.Light.DarkActionBar")]  // MainLauncher = true,
 
     public class TestRecycleActivity : Activity
-    {
-        // RecyclerView instance that displays the photo album:
+    {        
+        List<string> DisplayListSTRING = new List<string>();
+        List<NewsObject> DisplayListOBJECT = new List<NewsObject>();    
+        
+        // RecyclerView instance that displays the newsObject List:
         RecyclerView mRecyclerView;
 
         // Layout manager that lays out each card in the RecyclerView:
         RecyclerView.LayoutManager mLayoutManager;
 
-        // Adapter that accesses the data set (a photo album):
-        PhotoAlbumAdapter mAdapter;
-
-        // Photo album that is managed by the adapter:
-        PhotoAlbum mPhotoAlbum;
+        // Adapter that accesses the data set (List<newsObject>):
+        NewsObject_RecycleAdapter_NEW mAdapter;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Instantiate the photo album:
-            mPhotoAlbum = new PhotoAlbum();
+            //// Instantiate - taken from 'Get All Data' 
+            // clear List & get raw newsObject data from database
+            //ClearDisplayListAndAdapter();
+            //adapter.Clear();
+            DisplayListSTRING.Clear();
 
+            //RefreshTxtDataLastUpdated();  // not using in this experiment
+
+            DisplayListOBJECT.Clear();
+            DisplayListOBJECT = SetUpData.GetAllRawDataFromDatabase();
+
+            // convert this newsObject data to a List<string>
+            DisplayListSTRING = ConvertObjectsListToStringList(DisplayListOBJECT);
+            
             // Set our view from the "main" layout resource:
             SetContentView(Resource.Layout.TestRecycleLayout);
 
             // Get our RecyclerView layout:
-            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);      
+
 
             //............................................................
             // Layout Manager Setup:
@@ -57,133 +85,51 @@ namespace CurrencyAlertApp
             // Adapter Setup:
 
             // Create an adapter for the RecyclerView, and pass it the
-            // data set (the photo album) to manage:
-            mAdapter = new PhotoAlbumAdapter(mPhotoAlbum);
+            // data set (List<newsobject) to manage:
+            mAdapter = new NewsObject_RecycleAdapter_NEW(DisplayListOBJECT);
 
-            // Register the item click handler (below) with the adapter:
-            mAdapter.ItemClick += OnItemClick;
+            ////Register the item click handler(below) with the adapter:
+            //mAdapter.ItemClick += OnItemClick;
+            mAdapter.ItemClick += MAdapter_ItemClick;
+            mAdapter.ItemLongClick += MAdapter_ItemLongClick;
 
             // Plug the adapter into the RecyclerView:
-            mRecyclerView.SetAdapter(mAdapter);
-
-            //............................................................
-            // Random Pick Button:
-
-            // Get the button for randomly swapping a photo:
-            Button randomPickBtn = FindViewById<Button>(Resource.Id.randPickButton);
-
-            // Handler for the Random Pick Button:
-            randomPickBtn.Click += delegate
-            {
-                if (mPhotoAlbum != null)
-                {
-                    // Randomly swap a photo with the top:
-                    int idx = mPhotoAlbum.RandomSwap();
-
-                    // Update the RecyclerView by notifying the adapter:
-                    // Notify that the top and a randomly-chosen photo has changed (swapped):
-                    mAdapter.NotifyItemChanged(0);
-                    mAdapter.NotifyItemChanged(idx);
-                }
-            };
+            mRecyclerView.SetAdapter(mAdapter);           
         }// end onCreate
 
-        // Handler for the item click event:
-        void OnItemClick(object sender, int position)
+        private void MAdapter_ItemLongClick(object sender, NewsObject_RecycleAdapter_NEWClickEventArgs e)
         {
-            // Display a toast that briefly shows the enumeration of the selected photo:
-            int photoNum = position + 1;
+            int photoNum = e.Position + 1;
+            Toast.MakeText(this, "LONGGGGGGGGGGG Click\nThis is photo number " + photoNum, ToastLength.Short).Show();
+        }
+
+        private void MAdapter_ItemClick(object sender, NewsObject_RecycleAdapter_NEWClickEventArgs e)
+        {
+            int photoNum =  e.Position + 1;
             Toast.MakeText(this, "This is photo number " + photoNum, ToastLength.Short).Show();
         }
+
+
+
+        public List<string> ConvertObjectsListToStringList(List<NewsObject> newsObjects)
+        {
+            List<string> stringList = new List<string>();
+
+            foreach (var tempNewsObject in newsObjects)
+            {
+                // convert individual newsObject to a single string
+                string tempStringItem = (string.Format(
+                        "{0}:  {1}\nDate: {2}   {3} \n{4}",
+                        tempNewsObject.CountryChar.TrimEnd(),
+                        tempNewsObject.MarketImpact.TrimEnd(),
+                        tempNewsObject.DateAndTime.ToString("dd/MM/yyyy"),
+                        tempNewsObject.DateAndTime.ToString("HH:mm tt"),
+                        tempNewsObject.Name.TrimEnd()));
+
+                // add string to string list
+                stringList.Add(tempStringItem);
+            }
+            return stringList;
+        }
     }// end MainActivity
-
-
-
-    //----------------------------------------------------------------------
-    // VIEW HOLDER
-
-    // Implement the ViewHolder pattern: each ViewHolder holds references
-    // to the UI components (ImageView and TextView) within the CardView 
-    // that is displayed in a row of the RecyclerView:
-    public class PhotoViewHolder : RecyclerView.ViewHolder
-    {
-        public ImageView Image { get; private set; }
-        public TextView Caption { get; private set; }
-        public TextView MyCaption { get; set; }
-
-        // Get references to the views defined in the CardView layout.
-        public PhotoViewHolder(View itemView, Action<int> listener)
-            : base(itemView)
-        {
-            // Locate and cache view references:
-            Image = itemView.FindViewById<ImageView>(Resource.Id.imageView);
-            Caption = itemView.FindViewById<TextView>(Resource.Id.textView);
-            MyCaption = itemView.FindViewById<TextView>(Resource.Id.textView222);
-
-            // Detect user clicks on the item view and report which item
-            // was clicked (by layout position) to the listener:
-            itemView.Click += (sender, e) => listener(base.LayoutPosition);
-        }
-    }// end class PhotoViewHolder
-
-    //----------------------------------------------------------------------
-    // ADAPTER
-
-    // Adapter to connect the data set (photo album) to the RecyclerView: 
-    public class PhotoAlbumAdapter : RecyclerView.Adapter
-    {
-        // Event handler for item clicks:
-        public event EventHandler<int> ItemClick;
-
-        // Underlying data set (a photo album):
-        public PhotoAlbum mPhotoAlbum;
-
-        // Load the adapter with the data set (photo album) at construction time:
-        public PhotoAlbumAdapter(PhotoAlbum photoAlbum)
-        {
-            mPhotoAlbum = photoAlbum;
-        }
-
-        // Create a new photo CardView (invoked by the layout manager): 
-        public override RecyclerView.ViewHolder
-            OnCreateViewHolder(ViewGroup parent, int viewType)
-        {
-            // Inflate the CardView for the photo:
-            View itemView = LayoutInflater.From(parent.Context).
-                        Inflate(Resource.Layout.PhotoCardView, parent, false);
-
-            // Create a ViewHolder to find and hold these view references, and 
-            // register OnClick with the view holder:
-            PhotoViewHolder vh = new PhotoViewHolder(itemView, OnClick);
-            return vh;
-        }
-
-        // Fill in the contents of the photo card (invoked by the layout manager):
-        public override void
-            OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
-        {
-            PhotoViewHolder vh = holder as PhotoViewHolder;
-
-            // Set the ImageView and TextView in this ViewHolder's CardView 
-            // from this position in the photo album:
-            vh.Image.SetImageResource(mPhotoAlbum[position].PhotoID);
-            vh.Caption.Text = mPhotoAlbum[position].Caption;
-            vh.MyCaption.Text = mPhotoAlbum[position].Caption;
-        }
-
-        // Return the number of photos available in the photo album:
-        public override int ItemCount
-        {
-            get { return mPhotoAlbum.NumPhotos; }
-        }
-
-        // Raise an event when the item-click takes place:
-        void OnClick(int position)
-        {
-            if (ItemClick != null)
-                ItemClick(this, position);
-        }
-    }
-    
-
 }// end Namespace
